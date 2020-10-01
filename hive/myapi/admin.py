122267f63d5673f08d07757562c971d2fbca0cf7
EgeGurcan <ekula@wisc.edu>
2020-10-01 18:49:15 -0500

    a lot

--- hive/myapi/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(UserProfile)
-admin.site.register(Hive)
-admin.site.register(Inspection)
-admin.site.register(Equipment)
