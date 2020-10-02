5f41097336885d3da0915a5b186df98e50db0961
Roshan Pai <mpai@wisc.edu>
2020-10-01 21:27:47 -0500

    signup shit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,62 +1,112 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Nav from './components/Nav';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
+import './App.css';
 
 class App extends Component {
-  /*state = {
-    hives: []
-  }*/
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      error: null,
-      isLoaded: false,
-      hives: []
+      displayed_form: '',
+      logged_in: localStorage.getItem('token') ? true : false,
+      username: ''
     };
   }
 
   componentDidMount() {
-    fetch('http://127.0.0.1:8000/api/hives')
-    .then(res => res.json())
-    .then(
-      (data) => {
-        this.setState({
-          hives: data,
-          isLoaded: true
+    if (this.state.logged_in) {
+      fetch('http://localhost:8000/core/current_user/', {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('token')}`
+        }
+      })
+        .then(res => res.json())
+        .then(json => {
+          this.setState({ username: json.username });
         });
-      },
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      }
-    )
+    }
   }
 
+  handle_login = (e, data) => {
+    e.preventDefault();
+    fetch('http://localhost:8000/token-auth/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem('token', json.token);
+        this.setState({
+          logged_in: true,
+          displayed_form: '',
+          username: json.user.username
+        });
+      });
+  };
+
+  handle_signup = (e, data) => {
+    e.preventDefault();
+    fetch('http://127.0.0.1:8000/api/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem('token', json.token);
+        this.setState({
+          logged_in: true,
+          displayed_form: '',
+          username: json.username
+        });
+      });
+  };
+
+  handle_logout = () => {
+    localStorage.removeItem('token');
+    this.setState({ logged_in: false, username: '' });
+  };
+
+  display_form = form => {
+    this.setState({
+      displayed_form: form
+    });
+  };
+
   render() {
-    const {error, isLoaded, hives} = this.state;
-    if(error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded){
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div className="container">
-          <div className="col-xs-12">
-            <h1>My hives</h1>
-            {this.state.hives.map((hive) => (
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">{hive.name}</h5>
-                  <h6 className="card-subtitle mb-2 text-muted">
-                    {hive.addr}
-                  </h6>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      );
+    let form;
+    switch (this.state.displayed_form) {
+      case 'login':
+        form = <LoginForm handle_login={this.handle_login} />;
+        break;
+      case 'signup':
+        form = <SignupForm handle_signup={this.handle_signup} />;
+        break;
+      default:
+        form = null;
     }
+
+    return (
+      <div className="App">
+        <Nav
+          logged_in={this.state.logged_in}
+          display_form={this.display_form}
+          handle_logout={this.handle_logout}
+        />
+        {form}
+        <h3>
+          {this.state.logged_in
+            ? `Hello, ${this.state.username}`
+            : 'Please Log In'}
+        </h3>
+      </div>
+    );
   }
 }
 
