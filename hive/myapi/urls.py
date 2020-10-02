b2bc793871dfad7a5d50f93bad621cf9041e9a88
Vivian Dai <41034022+vdiavi@users.noreply.github.com>
2020-10-01 22:02:56 -0500

    Users View

--- hive/myapi/urls.py
+++ hive/myapi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
+from .views import current_user, UserList
 
 router = routers.DefaultRouter()
-##router.register(r'users', views.UserViewSet)
+#router.register(r'users', views.UserList)
 router.register(r'userprofiles', views.UserProfileViewSet)
 router.register(r'hives', views.HiveViewSet)
 router.register(r'inspections', views.InspectionViewSet)
@@ -11,5 +12,7 @@ router.register(r'equipment', views.EquipmentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('current_user/', current_user),
+    path('users/', UserList.as_view())
 ]
