87728429f32eb6654fc5a304ddef1e173fa524c6
cannycanny <cannycanny@CannyYangdeMacBook-Air.local>
2020-10-02 20:39:40 -0500

    enter

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -19,42 +19,48 @@ class LoginForm extends React.Component {
   };
 
   render() {
-   return (
-    <MDBContainer>
-      <MDBRow>
-        <MDBCol md="6">
-          <form onSubmit={e => this.props.handle_login(e, this.state)}>
-            <p className="h4 text-center mb-4">Sign in</p>
-            <label htmlFor="defaultFormLoginUsernameEx" className="grey-text">
-              Your username
-            </label>
-            <input
-              type="text"
-              name="username"
-              value={this.state.username}
-              onChange={this.handle_change}
-            />
-            <br />
-            <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
-              Your password
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handle_change}
-            />
-            <div className="text-center mt-4">
-              <MDBBtn color="indigo" type="submit">Login</MDBBtn>
-            </div>
-          </form>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-    );
-  }
-}
-
+    return (
+     <div style = {{
+         position: 'absolute', left: '50%', top: '50%',
+         transform: 'translate(-40%, -60%)'
+       }}>
+     <MDBContainer>
+       <MDBRow>
+         <MDBCol md="6">
+           <form onSubmit={e => this.props.handle_login(e, this.state)}>
+             {/* <p className="h4">Sign-in</p> */}
+             <label htmlFor="defaultFormLoginUsernameEx" className="grey-text">
+              Username
+             </label>
+             <input
+               type="text"
+               name="username"
+               value={this.state.username}
+               onChange={this.handle_change}
+             />
+             <br />
+             <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+               Password
+             </label>
+             <input
+               type="password"
+               name="password"
+               value={this.state.password}
+               onChange={this.handle_change}
+             />
+             <div className="text-center mt-4">
+               <MDBBtn color="warning" type="submit">Login</MDBBtn>
+             </div>
+           </form>
+         </MDBCol>
+       </MDBRow>
+     </MDBContainer>
+     </div>
+ 
+     );
+   }
+ }
+ 
 export default LoginForm;
 
 LoginForm.propTypes = {
